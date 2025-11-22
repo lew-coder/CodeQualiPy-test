5f1163fe84f92c4c0066f196c853e0f265d50bf1
lew-coder <lewiscairnsedwards@gmail.com>
2025-11-22 13:17:38 +0000

    Updated Director.py with latest changes

--- Qualipy/Director.py
+++ Qualipy/Director.py
@@ -1,11 +1,2 @@
-import sys
-
-from Qualipy.CCcomplexity import analyze as cc_analyze
-from Qualipy.CChalstead import analyze as h_analyze
-
-if __name__ == "__main__":
-    filepath = sys.argv[1]
-    print(f"== Cyclomatic Complexity ==")
-    cc_analyze(filepath)
-    print(f"== Halstead Metrics ==")
-    h_analyze(filepath)
+# Director File
+# To direct the script when multiple Qualipy Files have been created
